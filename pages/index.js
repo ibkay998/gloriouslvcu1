b25e0550efab6bc5a5c387d456250ca000f0e1b4
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2021-07-21 10:06:51 +0100

    added the grid layout and responsive design

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,51 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {
+  Header,
+  Text,
+  Grid,
+  GridItem,
+  Footer,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+} from "@chakra-ui/react"
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>GloriousLvcu</title>
+        <link rel="icon" href="/lvculogo.png" />
+        <Breadcrumb separator=" " className={styles.container1} ml={[5]}>
+          <BreadcrumbItem><BreadcrumbLink href="#">Home</BreadcrumbLink></BreadcrumbItem>
+          <BreadcrumbItem><BreadcrumbLink href="#">Home</BreadcrumbLink></BreadcrumbItem>
+          <BreadcrumbItem><BreadcrumbLink href="#">Home</BreadcrumbLink></BreadcrumbItem>
+          <BreadcrumbItem><BreadcrumbLink href="#">Home</BreadcrumbLink></BreadcrumbItem>
+          <BreadcrumbItem><BreadcrumbLink href="#">Home</BreadcrumbLink></BreadcrumbItem>
+          <BreadcrumbItem><BreadcrumbLink href="#">Home</BreadcrumbLink></BreadcrumbItem>
+        </Breadcrumb>
       </Head>
+      {/*  */}
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Grid 
+          w="100%"
+          templateColumns="repeat(3, 1fr)"
+          templateRows="repeat(6, 1fr)"
+        >
+          <GridItem w={[500,748,974,1257,1800]} rowSpan={1} colSpan={3}  h="100px" bg="red" >God is good and he his kind</GridItem>
+          <GridItem  h="50px" bg="blue"></GridItem>
+          <GridItem   h="100px" bg="green"></GridItem>
+          <GridItem  h="100px" bg="black"></GridItem>
+          <GridItem  h="100px" bg="purple"></GridItem>
+          <GridItem  h="100px" bg="blue"></GridItem>
+        </Grid>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      <footer className={styles.footer} bg="black">
+        <Text>Lagos Varsity Christian Union Copyright 2021. All rights reserved</Text>
       </footer>
     </div>
   )
